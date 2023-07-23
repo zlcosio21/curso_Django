97b83eebbe5ac8694ac7ae89dadaeffa6d91e8c6
Sebastian <sebas60621@gmail.com>
2023-07-23 11:50:23 -0500

    anadiendo variables y propiedades en plantillas

--- proyecto1/proyecto1/views.py
+++ proyecto1/proyecto1/views.py
@@ -2,15 +2,29 @@ from django.http import HttpResponse
 import datetime
 from django.template import Template, Context
 
-def saludo(request): # Primera vista
+class Persona:
+    def __init__(self, nombre, apellido, agno_nacimiento):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.agno_nacimiento = 2023 - agno_nacimiento
 
+def saludo(request): # Primera vista
     doc_externo = open(r"C:/Users/User/Desktop/curso_django/proyecto1/proyecto1/plantillas/plantilla1.html")
+
+    persona1 = Persona("Sebastian Andres", "Gomez Sobrino", 2006)
+
+    hora_actual = datetime.datetime.now()
 
     plt = Template(doc_externo.read())
 
     doc_externo.close()
 
-    ctx = Context()
+    ctx = Context({
+        "nombre_persona":persona1.nombre,
+        "apellido_persona":persona1.apellido,
+        "fecha_actual":hora_actual,
+        "edad":persona1.agno_nacimiento
+        })
 
     documento = plt.render(ctx)
 
