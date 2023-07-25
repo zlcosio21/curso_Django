060bca003fdf02c246a7c555bd102e220dad4e66
Sebastian <sebas60621@gmail.com>
2023-07-25 15:54:36 -0500

    Agregando filtros en el Panel de administracion

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -8,8 +8,14 @@ class cliente_admin(admin.ModelAdmin):
 class articulo_admin(admin.ModelAdmin):
     list_display = ("nombre", "seccion", "precio")
     search_fields = ("seccion", "nombre")
+    list_filter = ("seccion",)
+
+class pedido_admin(admin.ModelAdmin):
+    list_display = ("numero", "fecha", "entregado")
+    list_filter = ("fecha",)
+    date_hierarchy = "fecha"
 
 # Register your models here.
 admin.site.register(cliente, cliente_admin)
 admin.site.register(articulo, articulo_admin)
-admin.site.register(pedido)
+admin.site.register(pedido, pedido_admin)
