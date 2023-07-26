c61fefd733a639323b515cd8a9e4b9442e6db5d5
Sebastian <sebas60621@gmail.com>
2023-07-25 23:22:37 -0500

    Formularios conectadors a la BBDD

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gestionPedidos.models import articulo
 
 # Create your views here.
 def busqueda_productos(request):
@@ -7,7 +8,12 @@ def busqueda_productos(request):
     return render(request, "busqueda_productos.html")
 
 def buscar(request):
-
-    mensaje = "Articulo buscado: %r" %request.GET["producto"]
-
+    if request.GET["producto"]:
+       
+       #mensaje = "Articulo buscado: %r" %request.GET["producto"]
+       producto = request.GET["producto"]
+       articulos = articulo.objects.filter(nombre__icontains = producto)
+       return render(request, "resultado_busqueda.html", {"articulos":articulos, "query":producto,})
+    else:
+        mensaje = "No se ha encontrado nada"
     return HttpResponse(mensaje)
