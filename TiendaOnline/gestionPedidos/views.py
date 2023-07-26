95e8ec61847e4929b217c78076ebe173fbfc64f3
Sebastian <sebas60621@gmail.com>
2023-07-25 21:58:05 -0500

    Anadiendo template para realizar Formularios

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def busqueda_productos(request):
+
+    return render(request, "busqueda_productos.html")
+
+def buscar(request):
+
+    mensaje = "Articulo buscado: %r" %request.GET["producto"]
+
+    return HttpResponse(mensaje)
